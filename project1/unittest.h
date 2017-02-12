9187b7678a7bc2ba939a010e375c92100f238a59
Johan Fjeldtvedt <jaffe1@gmail.com>
2017-02-12 20:01:31 +0100

    add a kind of unit testing framework

---
+++ project1/unittest.h
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#define TEST_CASE(expr) \
+    printf("* test of %s:\t%s\n", #expr, (expr) ? "PASS" : "FAIL")
+
+#ifdef DO_UNITTEST
+#define UNITTEST(func) \
+    func();            \
+    return 0;
+#else
+#define UNITTEST(func) do { } while(0);
+#endif
+
+#ifdef DO_VTEST
+#define VERIFICATION_TEST(func) \
+    func();                     \
+    return 0;
+#else
+#define VERIFICATION_TEST(func) do { } while(0);
+#endif
+
+static inline bool within_range(double x, double y, double e)
+{
+    return abs(x - y) < e;
+}
